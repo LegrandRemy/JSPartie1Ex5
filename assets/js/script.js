13eacca5c4681b7cb0ba0324a1a07dcd4e87d141
LegrandRemy <legrandremy19@outlook.fr>
2021-07-27 09:35:58 +0200

    Exercice5

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function division() {
+let number1 = 12;
+let number2 = 5;
+result = number1 % number2;
+alert (`Le reste de la division \n${number1}  /  ${number2}  \n est = ${result} `);
+// restedivision = document.getElementById('division');
+}
+// addEventListener('click', division);restedivision.
